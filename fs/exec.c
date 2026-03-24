ceb53e18e76b7328534642b2cce77b21202e0935
NoCache-69 <chethangowdasj86@gmail.com>
2026-03-25 03:00:53 +0530

    treewide: Revert Old KernelSU-Next and susfs commits
    Revert "selinux: Allow init exec ksud under nosuid"
    
    This reverts commit 61958cbf1be9170dcb471fc9303210c28e8abcd7.
    
    Revert "fs: Backport path_umount for KernelSu"
    
    This reverts commit 59e6d8c0cd3ed88b5149fe995291722fbdf757a6.
    
    Revert "vendor/holi-qgki_defconfig: Unset KPROBES"
    
    This reverts commit 2a33a6e9edb6446a5c10a51d52cb682ee6033c1a.
    
    Revert "BACKPORT: seccomp: add filter_count field for KernelSU"
    
    This reverts commit 699a5b6280919b8a67c46e6328fa0be37991eaad.
    
    Revert "scripts: Don't append "-dirty" to localversion whatsoever"
    
    This reverts commit f19c2e846d7bd893c3d59c5c366fd39442c8c5fe.
    
    Revert "fs: Implement scope-minimized manual KernelSU calls/hooks with safe mode and abolish the dependence upon KProbes"
    
    This reverts commit 043df319d8c5b4af8f4449f6598d52bbb5f2bb95.
    
    Revert "KernelSU-Next: Import SuSFS v1.5.9 manually from k5.10"
    
    This reverts commit ed77117c39cd9761d2cabe35cdb4909140596023.
    
    Revert "Import KernelSU-Next submodule"
    
    This reverts commit dde0abafefdcac8a88b67054fa92ec2fecf4701b.

--- fs/exec.c
+++ fs/exec.c
@@ -68,10 +68,6 @@
 #include <asm/mmu_context.h>
 #include <asm/tlb.h>
 
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-#include <linux/susfs_def.h>
-#endif
-
 #include <trace/events/task.h>
 #include "internal.h"
 
@@ -1762,18 +1758,6 @@ static int __do_execve_file(int fd, struct filename *filename,
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-	if (likely(susfs_is_current_proc_su_not_allowed())) {
-		goto orig_flow;
-	}
-	if (likely(susfs_is_sus_su_hooks_enabled) &&
-		unlikely(__ksu_is_allow_uid(current_uid().val)))
-	{
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-	}
-orig_flow:
-#endif
-
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
 	 * set*uid() to execve() because too many poorly written programs
@@ -1920,13 +1904,6 @@ out_ret:
 	return retval;
 }
 
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-extern bool susfs_is_sus_su_hooks_enabled __read_mostly;
-extern bool __ksu_is_allow_uid(uid_t uid);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv,
-				void *envp, int *flags);
-#endif
-
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -2020,21 +1997,11 @@ void set_dumpable(struct mm_struct *mm, int value)
 	set_mask_bits(&mm->flags, MMF_DUMPABLE_MASK, value);
 }
 
-#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
-__attribute__((hot))
-extern int ksu_handle_execve_sucompat(int *fd,	const char __user **filename_user,
-				void *__never_use_argv,	void *__never_use_envp,
-				int *__never_use_flags);
-#endif
-
 SYSCALL_DEFINE3(execve,
 		const char __user *, filename,
 		const char __user *const __user *, argv,
 		const char __user *const __user *, envp)
 {
-#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
-	ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
-#endif
 	return do_execve(getname(filename), argv, envp);
 }
 
@@ -2056,9 +2023,6 @@ COMPAT_SYSCALL_DEFINE3(execve, const char __user *, filename,
 	const compat_uptr_t __user *, argv,
 	const compat_uptr_t __user *, envp)
 {
-#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK) // 32-bit ksud and 32-on-64 support
-	ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
-#endif
 	return compat_do_execve(getname(filename), argv, envp);
 }
 
